2cd0143760e441413403c797aaeddbd79de2c7ca
deimos <yrev2011@gmail.com>
2025-07-24 19:42:48 +0700

    Add get task list method on repository

--- src/core/task/repository.py
+++ src/core/task/repository.py
@@ -1,14 +1,20 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.task.entity import Task
+from src.core.task.filters import GetTaskListFilter
 from src.core.task.model import TaskModel
 
 
 class TaskRepository(ABC):
     @abstractmethod
     async def add_task(self, task: Task) -> None:
+        pass
+
+    @abstractmethod
+    async def get_task_list(self, filters: GetTaskListFilter) -> list[Task]:
         pass
 
 
@@ -20,3 +26,11 @@ class SQLAlchemyTaskRepository(TaskRepository):
         model = TaskModel.from_entity(task)
         self._session.add(model)
         await self._session.commit()
+
+    async def get_task_list(self, filters: GetTaskListFilter) -> list[Task]:
+        filter_dict = filters.to_dict()
+
+        query = select(TaskModel).filter_by(**filter_dict)
+        model_list = await self._session.scalars(query)
+
+        return [model.to_entity() for model in model_list.all()]
